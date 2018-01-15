3e81ea3d0221e8078708a655063c4b13456305b2
Tiger <1535725170@qq.com>
2018-01-16 00:25:50 +0800

    1/14
    界面调转打包与说明

---
+++ Drone/app/src/main/java/com/example/a1535725170/drone/SActivity.java
@@ -0,0 +1,21 @@
+package com.example.a1535725170.drone;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
+import android.transition.TransitionManager;
+
+/**
+ * Created by 1535725170 on 2018/1/15.
+ */
+
+public class SActivity extends Activity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sactivity);
+    }
+
+
+}
